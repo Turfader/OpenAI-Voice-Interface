2d5681722d9f71de408eaca16ad6422056589926
John Bouyer <jbouyer151@gmail.com>
2023-11-15 11:41:32 -0800

    put in a while loop

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ with open('.\\venv\\config.json') as config_file:
 
 
 client = OpenAI(api_key=api_key)
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+while True:
+    completion = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=[
+        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        {"role": "user", "content": input("Ask the AI something")}
+      ]
+    )
 
-print(completion.choices[0].message)
+    print(completion.choices[0].message)
