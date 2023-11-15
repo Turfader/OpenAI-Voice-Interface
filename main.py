17ca5cec5400e226ed1f08f3b4d1674280ed0fb6
John Bouyer <jbouyer151@gmail.com>
2023-11-15 11:36:34 -0800

    added test code

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello world")
+import json
+import openai
+from openai import OpenAI
+
+with open('.\\venv\\config.json') as config_file:
+    config = json.load(config_file)
+    api_key = config['API_KEY']
+
+
+client = OpenAI(api_key=api_key)
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
