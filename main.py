ffaddfb862a18cdbc980ea5c72153d7d3c6d85fb
Turfader <jbouyer151@gmail.com>
2023-11-15 23:48:05 -0800

    made the while loop actually loop now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import dotenv
-import openai
 from openai import OpenAI
 import os
 from text_to_speech import read
@@ -11,8 +10,8 @@ api_key = os.getenv("API_KEY")
 client = OpenAI(api_key=api_key)
 
 while True:
-    #user_input = input("Ask the AI something: ")
-    #user_input = 'This is a test. Please say "Hello world"'
+    # user_input = input("Ask the AI something: ")
+    # user_input = 'This is a test. Please say "Hello world"'
     user_input = transcribe_microphone()
     completion = client.chat.completions.create(
       model="gpt-3.5-turbo",
@@ -26,4 +25,4 @@ while True:
 
     print(completion.choices[0].message)
     read(str(completion.choices[0].message.content))
-    break
+    # break
